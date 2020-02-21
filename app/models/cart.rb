fe302239a4baedba864d14fdd1defba43d07fe01
howard <howard67890@gmail.com>
2020-02-21 10:20:35 +0800

    finished TDD Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,9 +1,49 @@
 # PORO = Plain Old Ruby Object
 class Cart
+  attr_reader :items
+
+  def initialize(items = [])
+    @items = items
+  end
+
+  def self.from_hash(hash = nil)
+    if hash && hash["items"]
+      items = hash["items"].map { |item|
+        CartItem.new(item["product_id"], item["quantity"])
+      }
+      Cart.new(items)
+    else
+      Cart.new
+    end
+  end
+
+  def serialize
+    # items = [
+    #   {"product_id" => 1, "quantity" => 3},
+    #   {"product_id" => 2, "quantity" => 2},
+    # ]
+
+    items = @items.map { |item| {"product_id" => item.product_id,
+                                 "quantity" => item.quantity} }
+
+    { "items" => items }
+  end
+
   def add_item(product_id)
+    found = @items.find { |item| item.product_id == product_id }
+    # 找...
+    if found
+      found.increment!
+    else
+      @items << CartItem.new(product_id)
+    end
   end
 
   def empty?
-    false
+    @items.empty?
+  end
+
+  def total_price
+    @items.reduce(0) { |sum, item| sum = sum + item.total_price }
   end
 end
