e38f836fd95f41617df4877b03f07305b49b700d
howard <howard67890@gmail.com>
2020-02-21 11:47:54 +0800

    finished Cart frontend(part-5)

--- app/controllers/api/v1/utils_controller.rb
+++ app/controllers/api/v1/utils_controller.rb
@@ -1,13 +1,24 @@
 class Api::V1::UtilsController < ApplicationController
   def subscribe
     email = params['subscribe']['email']
-    
     sub = Subscribe.new(email: email)
 
     if sub.save
       render json: { status: 'ok', email: email}
     else
       render json: { status: 'duplicated', email: email}
+    end
+  end
+
+  def cart
+    # 找到商品
+    product = Product.friendly.find(params[:id])
+
+    if product
+      current_cart.add_item(product.code)
+      session[:cart_9527] = current_cart.serialize
+
+      render json: {status: 'ok', items: current_cart.items.count}
     end
   end
 end
