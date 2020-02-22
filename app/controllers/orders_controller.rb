dbe8bb53c6405d1e2ba810b4f3f70bcc4b1b5241
howard <howard67890@gmail.com>
2020-02-22 13:29:24 +0800

    finished using Cart part 10

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,10 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   
   def create
-    @order = current_user.ordres.build(order_params)
+    @order = current_user.orders.build(order_params)
 
     current_cart.items.each do |item|
-      @order.order_items.build(sku: item.sku_id, quantity: item.quantity)
+      @order.order_items.build(sku_id: item.sku_id, quantity: item.quantity)
     end
 
     if @order.save
